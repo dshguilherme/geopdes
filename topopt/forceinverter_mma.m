60f7e5a8249d910458ff2cd25b0fc57fb368dcbd
Guilherme H. da Silva <dshguilherme@gmail.com>
2023-05-10 10:58:05 -0300

    added problems with mma

--- topopt/forceinverter_mma.m
+++ topopt/forceinverter_mma.m
@@ -110,11 +110,21 @@ u_drchlt = zeros(numel(drchlt_dofs),1); % For this case, homogeneous BCs.
 %% TopOpt Initial Conditions
 loop = 0;
 change = 1;
-xold1 = x;
-xold2 = x;
-low = ones(size(x));
-upp = ones(size(x));
-while change > 0.01
+% Initializing MMA Optimizer
+m = 1; % Number of general constraints
+nn = numel(density); % Number of variables to be optimized
+xmin = zeros(nn,1); % lower bounds for the variables
+xmax = ones(nn,1); % upper bounds for the variables
+xold1 = x(:); % xval one iteration ago if iter > 1
+xold2 = x(:); % xval two iterations ago if iter > 2
+low = ones(nn,1); % Vector with the lower asymptotes from the previous iter
+upp = ones(nn,1); % Vector with the upper asymptotes from the previous iter
+a0 = 1; % Constants a_0 in the term a_0*z
+a = zeros(m,1); % column vector with the constants a_i 
+c_MMA = 10000*ones(m,1); %column vector with the constants c_i*y_i
+d = zeros(m,1); %column vector with the constants 0.5*d_i*(y_i)^2
+
+while change > 0.01 && loop < 200
     loop = loop+1;
     % Pre-alocating vectors/solutions
     u = zeros(sp.ndof,1);
@@ -138,38 +148,47 @@ while change > 0.01
     adjoint_lambda(free_dofs) = K(free_dofs,free_dofs)\LL(free_dofs);
 %     adjoint_lambda = K\-LL;
     dc = zeros(msh.nel,1);
+    d2c = zeros(msh.nel,1);
     for e=1:msh.nel
         dofs = lm(e,:)';
         k_e = squeeze(Ke(e,:,:));
         lambda_ = adjoint_lambda(dofs);
         dc(e) = -3*(x(e)^2)*(E-Emin)*(lambda_'*k_e*u(dofs));
+        d2c(e) = -6*(x(e))*(E-Emin)*(lambda_'*k_e*u(dofs));
     end
     v_xe = x.*Ve;
     Re = dc./Ve;
-    xPhys = reshape(density,n);
+
     % Density filter
+    xPhys = reshape(density,n);
     dc_xy = reshape(dc,n);
+    d2c_xy = reshape(d2c,n);
     dc = conv2(dc_xy./Hs,h,'same');
+    d2c = conv2(d2c_xy./Hs,h,'same');
     dv = reshape(Ve,n);
     dv = conv2(dv./Hs,h,'same');
 
-    % Calc Lagrange Multiplier and update x
-    ell1 = 0; ell2 = 1e9; move = 0.1;
-    while(ell2-ell1)/(ell1+ell2) > 1e-4 && ell2 > 1e-40
-        mid = 0.5*(ell2+ell1);
-        xnew = max(0,max(x-move,min(1,min(x+move,x.*max(1e-10,-dc(:)./dv(:)/mid).^.3))));
-        xPhys = conv2(reshape(xnew,n),h,'same')./Hs;
-        if sum(xPhys(:)) > vol_frac*length(x)
-            ell1 = mid;
-        else
-            ell2 = mid;
-        end
-    end
-    change = max(abs(xnew(:)-x(:)));
-    density = xnew(:);
+  % Update x value through Method of Moving Asymptotes
+    xval = xPhys(:);
+    m = 1;
+    %nn = length(xval), iter = loop, xmin, xmax, xold1, xold2
+    f0val = compliance;
+    df0dx = dc(:);
+    df0dx2 = d2c(:);
+    fval = sum(xPhys(:))/(vol_frac*nn) -1;
+    dfdx = dv(:)'/(vol_frac*nn);
+    dfdx2 = zeros(m,nn);
+    [xmma,~,~,~,~,~,~,~,~,low,upp] = mmasub(m, nn, loop, xval, xmin, xmax, ...
+        xold1, xold2, f0val, df0dx, df0dx2, fval, dfdx, dfdx2, low, upp, ...
+        a0, a, c_MMA, d);
+    xPhys = conv2(reshape(xmma,n),h,'same')./Hs;
+    density = xmma(:);
+    xold2 = xold1;
+    xold1 = xval;
+    change = max(abs(xmma(:)-x(:)));
     x = xPhys(:);
     fprintf(' Iteration.:%5i | Compliance.:%11.2f | Vol.:%7.3f | Change.:%7.3f\n', ...
-        loop, compliance, mean(xnew(:)),change);
+        loop, compliance, mean(xmma(:)),change);
     colormap(gray); imagesc(xx,yy,1-rot90(xPhys)); caxis([0 1]); axis equal; axis off; drawnow;
 end
 
