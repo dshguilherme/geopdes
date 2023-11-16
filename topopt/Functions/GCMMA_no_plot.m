bf054c9a7d2f7f66145550e988ba4237d08fce37
Guilherme H. da Silva <dshguilherme@gmail.com>
2023-11-16 11:16:00 -0300

    autosave added

---
+++ topopt/Functions/GCMMA_no_plot.m
@@ -0,0 +1,81 @@
+function [xval, fobj, fres, x_history] = GCMMA_no_plot(f1,f2,init_mat, filter_options)
+    % Initialize
+    eval_objective_and_constraints = f1;
+    eval_objective = f2;
+    load(init_mat);
+    
+    
+    % Initial values for the functions
+    [f0val, df0dx, fval, dfdx] = eval_objective_and_constraints(xval);
+    change = 1;
+    x_history = zeros(numel(xval),maxoutit);
+    fobj = zeros(maxoutit,1);
+    fres = zeros(maxoutit,m);
+    % Start outer iterations
+    kktnorm = kkttol+10;
+    outit = 0;
+    while (kktnorm > kkttol) & (outit < maxoutit)  &  (change > change_min)
+        outit = outit+1;
+        outeriter = outeriter+1;
+    %     Calculate low, upp, raa0 and raa
+      [low,upp,raa0,raa] = ...
+      asymp(outeriter,n,xval,xold1,xold2,xmin,xmax,low,upp, ...
+            raa0,raa,raa0eps,raaeps,df0dx,dfdx);
+
+    % Solve GCMMA subproblem at xval
+      [xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,f0app,fapp] = ...
+      gcmmasub(m,n,outeriter,epsimin,xval,xmin,xmax,low,upp, ...
+               raa0,raa,f0val,df0dx,fval,dfdx,a0,a,c,d);
+    
+    % Calculate new function values at xmma
+    [f0valnew, fvalnew] = eval_objective(xmma);
+
+    % Check if it is conservative
+      [conserv] = concheck(m,epsimin,f0app,f0valnew,fapp,fvalnew);
+
+    % While the approximations are not conservative, make inner iterations
+    innerit = 0;
+    if conserv == 0
+        while conserv == 0 & innerit <= 15
+            innerit = innerit+1;
+    %         New values for raa0 and raa
+          [raa0,raa] = ...
+          raaupdate(xmma,xval,xmin,xmax,low,upp,f0valnew,fvalnew, ...
+                    f0app,fapp,raa0,raa,raa0eps,raaeps,epsimin);
+    %         GCMMA is solved with new raa0 and raa
+          [xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,f0app,fapp] = ...
+          gcmmasub(m,n,outeriter,epsimin,xval,xmin,xmax,low,upp, ...
+                   raa0,raa,f0val,df0dx,fval,dfdx,a0,a,c,d);
+               
+    %        Calculate the new fval and check if it is conservative
+          [f0valnew, fvalnew] = eval_objective(xmma);
+          [conserv] = concheck(m,epsimin,f0app,f0valnew,fapp,fvalnew);
+        end
+    end
+    % End of inner iterations. Update values
+    change = max(abs(xmma(:)-xval(:)));
+%     change = 100*abs((f0val-f0valnew)/f0val);
+%     change = max(abs(xmma(:)-xval(:)))/parameters.thickness;
+    xold2 = xold1;
+    xold1 = xval;
+    xval = xmma;
+    
+    % Filter new value
+    xPhys = apply_x_filter(filter_options, xmma);
+    xPhys = dfactor*xPhys +tmin;
+    % Calculate function and restrictions
+    [f0val, df0dx, fval, dfdx] = eval_objective_and_constraints(xval);
+    
+    % Residual vector of the KKT conditions:
+      [residu,kktnorm,residumax] = ...
+      kktcheck(m,n,xmma,ymma,zmma,lam,xsi,eta,mu,zet,s, ...
+               xmin,xmax,df0dx,fval,dfdx,a0,a,c,d);
+           
+    % Output Stuff
+        x_history(:,outit) = xval; x_plot = reshape(xPhys,nsub); fobj(outit) = f0val; fres(outit,:) = fval; 
+        fprintf(' Iteration: %3i | Objective: %3.1f | Mass: %4.1f kg | Restrictions: %3.1f | %3.1f | Change: %1.2e\n', ...
+      outit, f0val, RHO*sum(Ve.*xPhys), fval(1), fval(2), change);
+%     grafo = nrbplot(geometry.nurbs,nsub); colormap(jet); grafo.CData = x_plot; colorbar; drawnow; %imagesc(rot90(x_plot)); colorbar; caxis([0 100]); axis equal;axis off;drawnow;
+    end
+    fobj = fobj(1:outit); fres = fres(1:outit,:); x_history = x_history(:,1:outit);
+end
---
+++ topopt/shell_damping_autosave.m
@@ -0,0 +1,49 @@
+clearvars
+clc
+close all
+
+%% Initialization
+
+freqs = [15 60 102 125];
+names = {'square_v2rms_alpha_f15.mat','square_v2rms_alpha_f60.mat', 'square_v2rms_alpha_f102.mat', 'square_v2rms_alpha_f125.mat'};
+for ell=1:length(freqs)
+    problem_data = square_shell_problem;
+    % problem_data = scordelis_problem;
+    % problem_data = hemispherical_shell_problem(10, 10);
+    % Mesh parameters
+    parameters.degree = 2;
+    parameters.nsub = [80 80];
+
+    % Domain and Material properties
+    parameters.freq = freqs(ell);
+    parameters.omega = parameters.freq*2*pi;
+    parameters.RHO = 2700;
+    parameters.YOUNG = 69e12;
+    parameters.POISSON = 0.3;
+    parameters.alpha_ = 1.2*parameters.omega;
+    parameters.beta_ = 0; %0.1/parameters.omega;
+    parameters.Fmag = 1e6; % Force magnitude
+
+    % Optimization parameters
+    parameters.thickness = 0.1;
+    parameters.min_thickness = parameters.thickness/2;
+    parameters.max_thickness = parameters.thickness*2;
+    parameters.maximum_to_add = .80;
+    parameters.maximum_to_take = .50;
+
+    parameters.rmin = 4;
+    parameters.change_min = 1e-1;
+    parameters.iter_max = 400;
+    parameters.philter = "simple"; % 'simple' or 'density'
+    parameters.modo = "Continuous"; % 'SIMP' or 'Continuous'
+    parameters.neta = 0.9;
+    parameters.objective_function = "v2_rms";
+    %% Solve for initial step
+    initial_step_KL_shell(parameters, problem_data);
+    load('init_shell.mat')
+    %% Optimization
+[xval, fobj, fres, x_history] = ...
+    GCMMA_no_plot(f1, f2, 'init_shell.mat', filter_options);
+clearvars -except freqs ell names xval fobj fres x_history
+save(names{ell})
+end
